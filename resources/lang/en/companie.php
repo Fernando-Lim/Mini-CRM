6cdf380208788dcaa9a2d66cbd699ffa875b3f03
Fernando-Lim <72370746+Fernando-Lim@users.noreply.github.com>
2022-01-12 20:13:23 +0700

    Add files via upload

---
+++ resources/lang/en/companie.php
@@ -0,0 +1,29 @@
+<?php 
+
+return [
+    
+    'title1' => 'Companie',
+    'title2' => 'Add Companie',
+    'title3' => 'Edit Companie',
+
+    'table1' => 'Name',
+    'table2' => 'Email',
+    'table3' => 'Logo',
+    'table4' => 'Website',
+    'table5' => 'Action',
+
+    'status1' => 'No Data',
+    'status2' => 'Companie Added Sucessfully!',
+    'status3' => 'Companie Updated Sucessfully!',
+    'status4' => 'Companie Deleted Successfully!',
+
+    'btn1' => 'Add Companie',
+    'btn2' => 'Edit',
+    'btn3' => 'Delete',
+    'btn4' => 'Back',
+    'btn5' => 'Edit Companie',
+
+    'alert' => 'Are You Sure?',
+];
+
+?>
